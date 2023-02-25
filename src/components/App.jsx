1b5289ec04d85de2ea9d146dddfa9c2f0ac29d3f
DawidKrupski <dawid.krupski323@gmail.com>
2023-02-25 17:36:54 +0100

    pagination

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import axios from 'axios';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
 import { Searchbar } from './Searchbar/Searchbar';
 
 const API_key = '13558836-548568db06f41293b437b04a2';
-let search = '';
-let url = `https://pixabay.com/api/?q=${search}&page=1&key=${API_key}&image_type=photo&orientation=horizontal&per_page=12`;
 
 export class App extends React.Component {
   state = {
     search: '',
     images: [],
+    currentPage: 1,
   };
 
-  handleSubmit = async evt => {
-    evt.preventDefault();
-    const url = `https://pixabay.com/api/?q=${this.state.search}&page=1&key=${API_key}&image_type=photo&orientation=horizontal&per_page=12`;
+  async getURL() {
+    const url = `https://pixabay.com/api/?q=${this.state.search}&page=${this.state.currentPage}&key=13558836-548568db06f41293b437b04a2&image_type=photo&orientation=horizontal&per_page=12`;
     const response = await axios.get(url);
     this.setState({
       images: response.data.hits,
     });
+  }
+
+  handleSubmit = async evt => {
+    evt.preventDefault();
+    this.getURL();
+  };
+
+  handleNextPage = evt => {
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+    });
+    console.log(this.state.currentPage);
+  };
+
+  handlePrevPage = evt => {
+    this.setState({
+      currentPage: this.state.currentPage - 1,
+    });
+    console.log(this.state.currentPage);
   };
 
   handleInput = evt => {
@@ -29,16 +47,15 @@ export class App extends React.Component {
   };
 
   async componentDidMount() {
-    const response = await axios.get(url);
-    this.setState({
-      images: response.data.hits,
-    });
+    this.getURL();
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.search.length !== this.state.search) {
+      this.getURL();
     }
   }
+
   render() {
     const { images } = this.state;
     return (
@@ -48,6 +65,11 @@ export class App extends React.Component {
           handleSubmit={this.handleSubmit}
         />
         <ImageGallery images={images} />
+        <Button
+          currentPage={this.state.currentPage}
+          handleNextPage={this.handleNextPage}
+          handlePrevPage={this.handlePrevPage}
+        />
       </>
     );
   }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import css from './Button.module.css';
 
 export class Button extends React.Component {
   render() {
-    return '';
+    const { handleNextPage, handlePrevPage, currentPage } = this.props;
+    return (
+      <>
+        {currentPage > 1 ? (
+          <button onClick={handlePrevPage} className={css.button}></button>
+        ) : (
+          ''
+        )}
+        {currentPage < 41 ? (
+          <button onClick={handleNextPage} className={css.button}></button>
+        ) : (
+          ''
+        )}
+      </>
+    );
   }
 }
