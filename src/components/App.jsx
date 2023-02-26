40092d8d066c73c24e5eee3c87b151bec22bdc91
DawidKrupski <dawid.krupski323@gmail.com>
2023-02-26 09:31:14 +0100

    Loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Searchbar } from './Searchbar/Searchbar';
+import { Loader } from './Loader/Loader';
 
 const API_key = '13558836-548568db06f41293b437b04a2';
 
@@ -11,33 +12,50 @@ export class App extends React.Component {
     search: '',
     images: [],
     currentPage: 1,
+    perPage: 12,
+    error: null,
+    isLoading: false,
   };
 
   async getURL() {
-    const url = `https://pixabay.com/api/?q=${this.state.search}&page=${this.state.currentPage}&key=${API_key}&image_type=photo&orientation=horizontal&per_page=12`;
-    const response = await axios.get(url);
-    this.setState({
-      images: response.data.hits,
-    });
+    const url = `https://pixabay.com/api/?q=${this.state.search}&page=${this.state.currentPage}&key=${API_key}&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`;
+    this.setState({ isLoading: true });
+    try {
+      const response = await axios.get(url);
+      this.setState({
+        images: response.data.hits,
+      });
+    } catch (error) {
+      this.setState({
+        error: error.message,
+      });
+    } finally {
+      this.setState({
+        isLoading: false,
+      });
+    }
   }
 
   handleSubmit = async evt => {
     evt.preventDefault();
+    await this.setState({
+      currentPage: 1,
+    });
     this.getURL();
   };
 
-  handleNextPage = evt => {
-    this.setState({
+  handleNextPage = async evt => {
+    await this.setState({
       currentPage: this.state.currentPage + 1,
     });
-    console.log(this.state.currentPage);
+    this.getURL();
   };
 
-  handlePrevPage = evt => {
-    this.setState({
+  handlePrevPage = async evt => {
+    await this.setState({
       currentPage: this.state.currentPage - 1,
     });
-    console.log(this.state.currentPage);
+    this.getURL();
   };
 
   handleInput = evt => {
@@ -50,26 +68,32 @@ export class App extends React.Component {
     this.getURL();
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.search.length !== this.state.search) {
-      this.getURL();
+  renderImages = () => {
+    if (this.state.isLoading) {
+      return <Loader />;
+    } else if (!this.state.isLoading && this.state.images.length > 0) {
+      return <ImageGallery images={this.state.images} />;
+    } else if (this.state.error && !this.state.isLoading) {
+      return <div>Something went wrong: {this.state.error}</div>;
     }
-  }
+  };
 
   render() {
-    const { images } = this.state;
     return (
       <>
         <Searchbar
           handleInput={this.handleInput}
           handleSubmit={this.handleSubmit}
         />
-        <ImageGallery images={images} />
-        <Button
-          currentPage={this.state.currentPage}
-          handleNextPage={this.handleNextPage}
-          handlePrevPage={this.handlePrevPage}
-        />
+        {this.renderImages()}
+        {!this.state.isLoading ? (
+          <Button
+            currentPage={this.state.currentPage}
+            handleNextPage={this.handleNextPage}
+            handlePrevPage={this.handlePrevPage}
+            perPage={this.state.perPage}
+          />
+        ) : null}
       </>
     );
   }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,20 +3,24 @@ import css from './Button.module.css';
 
 export class Button extends React.Component {
   render() {
-    const { handleNextPage, handlePrevPage, currentPage } = this.props;
+    const { handleNextPage, handlePrevPage, currentPage, perPage } = this.props;
     return (
-      <>
+      <div className={css.buttons}>
         {currentPage > 1 ? (
-          <button onClick={handlePrevPage} className={css.button}></button>
+          <button onClick={handlePrevPage} className={css.button}>
+            Previous Page
+          </button>
         ) : (
           ''
         )}
-        {currentPage < 41 ? (
-          <button onClick={handleNextPage} className={css.button}></button>
+        {currentPage < 500 / perPage ? (
+          <button onClick={handleNextPage} className={css.button}>
+            Next Page
+          </button>
         ) : (
           ''
         )}
-      </>
+      </div>
     );
   }
 }
